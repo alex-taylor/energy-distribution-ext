0b048e485a8024b9cab488c9b3f783b759234fc5
alex-taylor <alex@nightshade.org.uk>
2026-03-11 20:57:26 +0000

    Cleaned up the build scripts

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,8 @@ import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import serve from "rollup-plugin-serve";
-import terser  from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import minifyHTML from 'rollup-plugin-minify-html-literals';
-
-const dev = process.env.ROLLUP_WATCH;
-
-const serveOptions = {
-  contentBase: ["./dist"],
-  host: "0.0.0.0",
-  port: 5000,
-  allowCrossOrigin: true,
-  headers: {
-    "Access-Control-Allow-Origin": "*"
-  }
-};
 
 export default [
   {
@@ -31,7 +17,6 @@ export default [
     ],
     plugins: [
       minifyHTML(),
-      //terser({ output: { comments: false } }),
       typescript(),
       nodeResolve(),
       json({
@@ -41,18 +26,7 @@ export default [
       babel({
         exclude: "node_modules/**",
         babelHelpers: "bundled"
-      }),
-      ...(dev ? [serve(serveOptions)] : [/*terser()*/])
-    ],
-    moduleContext: (id) => {
-      const thisAsWindowForModules = [
-        "node_modules/@formatjs/intl-utils/lib/src/diff.js",
-        "node_modules/@formatjs/intl-utils/lib/src/resolve-locale.js",
-      ];
-
-      if (thisAsWindowForModules.some((id_) => id.trimRight().endsWith(id_))) {
-        return "window";
-      }
-    }
+      })
+    ]
   }
 ];
